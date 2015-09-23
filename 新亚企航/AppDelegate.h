bfaae0c4f6cf2c1e857263a23bfa8c719e999477
dorayo <yomingyo@gmail.com>
2015-09-23 17:30:27 +0800

    Initial Commit

---
+++ 新亚企航/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  新亚企航
+//
+//  Created by qingyun on 15/9/23.
+//  Copyright (c) 2015年 qingyun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 新亚企航/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  新亚企航
+//
+//  Created by qingyun on 15/9/23.
+//  Copyright (c) 2015年 qingyun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
